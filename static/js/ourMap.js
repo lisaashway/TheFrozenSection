21127c663401133c6c46e0295d06b63f2379e739
Asher C <asherrc@gmail.com>
2020-07-12 19:09:58 -0400

    pushing maps

--- static/js/ourMap.js
+++ static/js/ourMap.js
@@ -1,3 +1,5 @@
+
+
 // Adding tile layer
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
