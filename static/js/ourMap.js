474af069a7e835a540dadc6d88991df5edcb28a2
lisaashway <lisaashway@gmail.com>
2020-07-13 21:40:06 -0400

    added deployment needed

--- static/js/ourMap.js
+++ static/js/ourMap.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> ccfe8dc2e5e7ca2a6504f6b539ecba85863074c9
 // Adding tile layer
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
